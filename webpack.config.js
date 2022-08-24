1cc24181ee75685832c4873700e7eef6e4c3c352
zxw1027 <zhang.xiaowei23@iwhalecloud.com>
2022-08-24 17:49:10 +0800

    webpack管理输出

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js',
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '管理输出',
+        }),
+    ],
+
     output: {
-        // filename: 'main.js',
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,//每次构建清理前一次构建的内容
     },
     //
     module: {
